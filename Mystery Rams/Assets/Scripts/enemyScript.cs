14f63f1a631ca46fabbd6de62f8c135467ba92ee
ASMurren <123965107+ASMurren@users.noreply.github.com>
2023-07-01 16:33:04 -0400

    Created 4 Scripts
    Created Bullets,ButtonFunctions,IDamage, and GameManager Scripts. Completed Bullets and IDamage

--- Mystery Rams/Assets/Scripts/enemyScript.cs
+++ Mystery Rams/Assets/Scripts/enemyScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemyScript : MonoBehaviour//, damage script implementation
+public class enemyScript : MonoBehaviour, IDamage//, damage script implementation
 {
 
     //Currently doesn't react to physics.
