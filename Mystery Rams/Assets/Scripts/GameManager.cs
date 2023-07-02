84b3ecd85c7fbf63ac99833fd743a87267859a3d
ASMurren <123965107+ASMurren@users.noreply.github.com>
2023-07-02 01:18:15 -0400

    GameManager Update
    Game Manager Script

--- Mystery Rams/Assets/Scripts/GameManager.cs
+++ Mystery Rams/Assets/Scripts/GameManager.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
+    [Header("Menu UI")]
+    public GameObject activeMenu;
+    public GameObject pauseMenu;
+    public GameObject winMenu;
+    public GameObject loseMenu;
+    public TextMeshProUGUI enemiesRemainingText;
+    public GameObject playerDamageIndicator;
+
+    int enemiesRemaining;
+    bool isPaused;
+    float timeScaleOrig;
+   
     void Start()
     {
         
@@ -14,6 +25,58 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Cancel") && activeMenu == null) 
+        {
+        PauseGame();
+            activeMenu = pauseMenu;
+            activeMenu.SetActive(isPaused);
         
+        }
+        
+    }
+
+    public void PauseGame() 
+    {
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+        isPaused = !isPaused;
+    
+    }
+
+    public void GameUnpaused() 
+    {
+        Time.timeScale = timeScaleOrig;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        isPaused = !isPaused;
+        activeMenu.SetActive(false);
+        activeMenu = null;
+    
+    }
+    public void UpdateGameGoal(int amount)
+    {
+        enemiesRemaining += amount;
+        enemiesRemainingText.text = enemiesRemaining.ToString("F0");
+        if(enemiesRemaining <= 0)
+        {
+            activeMenu = winMenu;
+            activeMenu.SetActive(true);
+            PauseGame();
+        }
+    }
+    public void GameLoss()
+    {
+        PauseGame();
+        activeMenu = loseMenu;
+        activeMenu.SetActive(true);
+    }
+    public IEnumerator playerDamageIndication() 
+    {
+    playerDamageIndicator.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        playerDamageIndicator.SetActive(false);
+    
+    
     }
 }
