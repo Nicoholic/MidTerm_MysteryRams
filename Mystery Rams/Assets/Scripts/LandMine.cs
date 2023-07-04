45ea749d79f9433260a4a6b5cf0ac9b9b7e05b37
ASMurren <123965107+ASMurren@users.noreply.github.com>
2023-07-04 03:12:33 -0400

    script and scene work
    created LandMine Script(will finish later)

---
+++ Mystery Rams/Assets/Scripts/LandMine.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandMine : MonoBehaviour
+{
+
+    //Do not touch i will finish this
+   
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
