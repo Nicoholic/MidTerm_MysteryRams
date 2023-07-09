5a06b420b62227a2331a24ebbc95687572e477dc
ASMurren <123965107+ASMurren@users.noreply.github.com>
2023-07-08 23:12:27 -0400

    Script work
    Moving Platform Scripts

---
+++ Mystery Rams/Assets/Scripts/MovingPlatformPath.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformPath : MonoBehaviour
+{
+   public Transform GetPoint(int pointIndex) 
+    {
+    
+    return transform.GetChild(pointIndex);
+    }
+
+    public int GetNextPointIndex(int currentPointIndex) 
+    {
+    int nextPointIndex = currentPointIndex + 1;
+    if (nextPointIndex == transform.childCount)
+        {
+        
+        nextPointIndex = 0;
+        
+        }
+    return nextPointIndex;
+    
+    }
+}
