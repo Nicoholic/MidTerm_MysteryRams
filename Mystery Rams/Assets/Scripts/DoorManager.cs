7c2f75a046dfd66cfa491cc561a2aae954c82c46
ASMurren <123965107+ASMurren@users.noreply.github.com>
2023-07-19 10:56:19 -0400

    Script work
    Door Manager Script

---
+++ Mystery Rams/Assets/Scripts/DoorManager.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+    public bool isOpen = false;
+    [SerializeField] bool isDoor;
+
+    [Header("Door Settings")]
+    [SerializeField] float doorSpeed = 1F;
+    [SerializeField] float doorRotationAngle = 90F;
+    [SerializeField] float openingDirection = 0F;
+
+    private Vector3 initialRotation;
+    private Vector3 forward;
+
+    private Coroutine doorAnimation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialRotation = transform.rotation.eulerAngles;
+        forward = transform.right;
+    }
+
+    public void Open(Vector3 playerPosition)
+    {
+
+        if (!isOpen)
+        {
+
+            if (doorAnimation != null)
+            {
+
+                StopCoroutine(doorAnimation);
+
+            }
+            if (isDoor)
+            {
+                float dotProduct = Vector3.Dot(forward, (playerPosition - transform.position).normalized);
+                doorAnimation = StartCoroutine(OpenDoor(dotProduct));
+
+            }
+
+        }
+
+    }
+
+    private IEnumerator OpenDoor(float forwardAmount)
+    {
+        Quaternion startRotation = transform.rotation;
+        Quaternion endRotation;
+
+        if (forwardAmount >= openingDirection)
+        {
+            endRotation = Quaternion.Euler(new Vector3(0, initialRotation.y - doorRotationAngle, 0));
+
+        }
+        else
+        {
+            endRotation = Quaternion.Euler(new Vector3(0, initialRotation.y + doorRotationAngle, 0));
+
+        }
+
+        isOpen = true;
+        float time = 0;
+        while (time < 1)
+        {
+            transform.rotation = Quaternion.Slerp(startRotation, endRotation, time);
+            yield return null;
+            time += Time.deltaTime * doorSpeed;
+        }
+
+    }
+
+    public void Close() 
+    {
+        if (isOpen) 
+        {
+        if(doorAnimation != null) 
+            {
+                StopCoroutine(doorAnimation);
+            
+            }
+        if(isDoor) 
+            {
+                doorAnimation = StartCoroutine(CloseDoor());
+           
+            }
+        
+        }
+
+    }
+
+    private IEnumerator CloseDoor() 
+    {
+
+        Quaternion startRotation_ = transform.rotation;
+        Quaternion endRotation = Quaternion.Euler(initialRotation);
+
+        isOpen = false;
+        float time = 0;
+        while (time < 1)
+        {
+
+            transform.rotation = Quaternion.Slerp(startRotation_, endRotation, time);
+            yield return null;
+            time += Time.deltaTime * doorSpeed;
+
+        }
+
+    }
+
+}
