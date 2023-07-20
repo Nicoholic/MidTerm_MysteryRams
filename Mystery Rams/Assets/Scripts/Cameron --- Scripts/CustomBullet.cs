1fa1d753b757e4fbb41fb6ad93c70d00170a21b5
Cameron Caulkins <CamoCaulkins@gmail.com>
2023-07-20 09:17:09 -0400

    something

--- Mystery Rams/Assets/Scripts/Cameron --- Scripts/CustomBullet.cs
+++ Mystery Rams/Assets/Scripts/Cameron --- Scripts/CustomBullet.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
-using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.SubsystemsImplementation;
 
--- Mystery Rams/Assets/Scripts/SetFullScreen.cs
+++ Mystery Rams/Assets/Scripts/SetFullScreen.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Search.Providers;
 using UnityEngine;
 
 public class SetFullScreen : MonoBehaviour
