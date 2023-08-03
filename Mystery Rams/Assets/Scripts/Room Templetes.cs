570a68cff5088f698463260b65d91c59607cd3f8
Nicoholic <Harlamv43@gmail.com>
2023-08-03 17:25:16 -0400

    Map Gen
    Started on the auto map
    
    added spawners
    scripts
    
    the room will spawn the end pieces correctly
    T/S the other rooms right now

---
+++ Mystery Rams/Assets/Scripts/Room Templetes.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTempletes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] northRooms;
+    public GameObject[] eastRooms;
+    public GameObject[] southRooms;
+    public GameObject[] westRooms;
+}
